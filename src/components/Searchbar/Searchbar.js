fc78941e5b09234e8b483aea4bde0b9f4c45da4d
alexander-kulyk <kulyk.alexander@gmail.com>
2022-12-15 19:49:15 +0200

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,55 +1,106 @@
-import { Component } from "react";
 import PropTypes  from "prop-types";
+import { useState } from "react";
 import { Form, Header, Input, NameBtn, SearchBtn } from "./Searchbar.styled";
 
 
-export class Searchbar extends Component{
-    state = {
-        query:''
+
+export const Searchbar = ({onSubmit}) =>{
+    const [query, setQuery] = useState('');
+
+    const handelChangeInput = e =>{
+        
+        setQuery(e.target.value)
     }
 
-
-    handelChangeInput = e =>{
-        this.setState({query: e.target.value})
-    }
-    handleSubmitForm = e =>{
+    const  handleSubmitForm = e =>{
         e.preventDefault();
         // console.log(e.currentTarget.elements.query.value)
-        const { query } = this.state;
-        const { onSubmit } = this.props;
         onSubmit(query);
 
-        this.reset()
+        reset()
 
-    }
-    reset = () =>{
-        this.setState({query: '',})
-    }
+    };
 
-    render(){
-        const { query } = this.state
+
+    const  reset = () =>{
+        setQuery('')
+    }
+    
+    
+    return(
+        <Header>
+            <Form onSubmit={handleSubmitForm}>
+                <Input 
+                    type='text'
+                    name="query"
+                    autocomplete="off"
+                    placeholder="Search images and photos"
+                    value={query}
+                    onChange={handelChangeInput}
+                />
+                <SearchBtn type="submit">
+                    <NameBtn>Search</NameBtn>
+                </SearchBtn>
         
-        return(
-            <Header>
-                <Form onSubmit={this.handleSubmitForm}>
-                    <Input 
-                        type='text'
-                        name="query"
-                        autocomplete="off"
-                        placeholder="Search images and photos"
-
-                        value={query}
-                        onChange={this.handelChangeInput}
-                    />
-                    <SearchBtn type="submit">
-                        <NameBtn>Search</NameBtn>
-                    </SearchBtn>
-
-                </Form>
-            </Header>
-        )
-    }
+            </Form>
+        </Header>
+    )
 }
+
+
+
+
+
+
+
+
+// export class Searchbar extends Component{
+//     state = {
+//         query:''
+//     }
+
+
+//     handelChangeInput = e =>{
+//         this.setState({query: e.target.value})
+//     }
+//     handleSubmitForm = e =>{
+//         e.preventDefault();
+//         // console.log(e.currentTarget.elements.query.value)
+//         const { query } = this.state;
+//         const { onSubmit } = this.props;
+//         onSubmit(query);
+
+//         this.reset()
+
+//     }
+//     reset = () =>{
+//         this.setState({query: '',})
+//     }
+
+//     render(){
+//         const { query } = this.state
+        
+//         return(
+//             <Header>
+//                 <Form onSubmit={this.handleSubmitForm}>
+//                     <Input 
+//                         type='text'
+//                         name="query"
+//                         autocomplete="off"
+//                         placeholder="Search images and photos"
+
+//                         value={query}
+//                         onChange={this.handelChangeInput}
+//                     />
+//                     <SearchBtn type="submit">
+//                         <NameBtn>Search</NameBtn>
+//                     </SearchBtn>
+
+//                 </Form>
+//             </Header>
+//         )
+//     }
+// }
 
 Searchbar.propTypes = {
     onSubmit: PropTypes.func.isRequired,
